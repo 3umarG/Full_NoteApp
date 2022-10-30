19a75382f1797ca87ee8655b93b1b5d8c11e6919
OmarGomaaFCi <omargomaafcitu@gmail.com>
2022-10-31 00:25:12 +0200

    Dao Commit

---
+++ app/src/main/java/com/example/noteapp/data/local/NoteDao.kt
@@ -0,0 +1,29 @@
+package com.example.noteapp.data.local
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.noteapp.models.Note
+
+@Dao
+interface NoteDao {
+
+    // Insert
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertNote(note : Note)
+
+    // Update
+    @Update
+    suspend fun updateNote(note: Note)
+
+    // Delete
+    @Delete
+    suspend fun deleteNote(note: Note)
+
+    // Get All
+    @Query("SELECT * FROM notes ORDER BY id ASC")
+    fun getAllNotes() : LiveData<List<Note>>
+
+    // Search on Note by using title / content
+    @Query("SELECT * FROM NOTES WHERE title LIKE :query OR content LIKE :query ORDER BY id ASC ")
+    fun searchNote(query: String) : LiveData<List<Note>>
+}
